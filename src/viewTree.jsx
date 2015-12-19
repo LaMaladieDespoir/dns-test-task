4a2ae0c1f4dce0e30b515f55903969011a6b9f45
gaparchi <peh626@gmail.com>
2015-12-19 19:29:28 +1000

    добавил рендеринг данных на клиенте

--- src/viewTree.jsx
+++ src/viewTree.jsx
@@ -1,9 +1,42 @@
 var React = require('react');
-var $ = require('jquery-browserify')
+var $ = require('jquery-browserify');
 
 var DnsTree = React.createClass({
+	getInitialState: function() {
+		return {tree:[]};
+	},
+	componentDidMount: function() {
+		this.refreshData();
+	},
+	refreshData: function(){
+		$.getJSON('/ajax.php',{action:'get'},function(data) {
+			console.log(data);
+			this.setState({
+				tree:data,
+			});
+		}.bind(this));
+	},
+	handlerFolderClick: function(event) {
+		$(event.target).next('ul').toggle();
+	},
+	loopCategory: function(categories,files){
+		return (<ul>{categories.map(function(category,ikey){
+						var child;
+						if (category.child.length || category.files.length) {
+							child = this.loopCategory(category.child,category.files);
+						}else{
+							child = null;
+						}
+						return (<li key={ikey}><b onClick={this.handlerFolderClick}>{category.name}</b>{child}</li>);
+					}.bind(this))}
+
+					{files.map(function(file,ikey){
+						return (<li key={ikey+100}>{file.name}</li>);
+					}.bind(this))}
+			</ul>);
+	},
 	render: function() {
-		return <h1>Hello</h1>;
+		return (<div>{this.loopCategory(this.state.tree,[])}</div>);
 	}
 });
 
